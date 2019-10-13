5351f71eb144c133df36feaa9bd34c3d1bcc5691
jdnichollsc <jdnichollsc@hotmail.com>
2019-10-13 15:19:53 -0500

    Fix README using ion-phaser-ce tag instead

--- react/rollup.config.js
+++ react/rollup.config.js
@@ -2,32 +2,19 @@ import resolve from 'rollup-plugin-node-resolve';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import commonjs from 'rollup-plugin-commonjs';
 
-// import path from 'path';
-// const phaserModule = path.join(__dirname, './node_modules/phaser-ce/');
-// const pixi = path.resolve(phaserModule, 'phaser-ce/build/custom/pixi');
-// const p2 = path.resolve(phaserModule, 'phaser-ce/build/custom/p2');
-
 export default {
   input: 'dist-transpiled/index.js',
   output: [
     {
       file: 'dist/index.esm.js',
       format: 'es',
-      sourcemap: true,
-      globals: {
-        'phaser-ce/build/custom/pixi': 'PIXI',
-        'phaser-ce/build/custom/p2': 'p2'
-      }
+      sourcemap: true
     },
     {
       file: 'dist/index.js',
       format: 'commonjs',
       preferConst: true,
-      sourcemap: true,
-      globals: {
-        'phaser-ce/build/custom/pixi': 'PIXI',
-        'phaser-ce/build/custom/p2': 'p2'
-      }
+      sourcemap: true
     }
   ],
   external: [
@@ -35,8 +22,6 @@ export default {
     '@ion-phaser-ce/core/loader',
     'react',
     'tslib',
-    'phaser-ce/build/custom/pixi',
-    'phaser-ce/build/custom/p2',
     'phaser-ce'
   ],
   plugins: [
